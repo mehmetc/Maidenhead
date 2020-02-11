7464b2ca9f221dc64836824372e2da95862fa3e6
Paul Irwin <paulirwin@gmail.com>
2020-02-11 08:30:01 -0700

    Initial port to TypeScript

--- src/maidenhead.ts
+++ src/maidenhead.ts
@@ -1,12 +1,20 @@
-"use strict";
-module.exports = class Maidenhead {
-    constructor(lat, lon, precision = 5){
+
+export default class Maidenhead {
+    private _lat: number | undefined;
+    private _lon: number | undefined;
+    private _precision: number = 5;
+    private _locator: string | undefined;
+    private __lat: number | undefined;
+    private __lon: number | undefined;
+    private __precision: number | undefined;
+
+    constructor(lat: number, lon: number, precision = 5) {
         this.lat = lat;
         this.lon = lon;
         this.precision = precision;
     }
 
-    static valid(mlocation){
+    static valid(mlocation: string) {
         if (typeof mlocation !== 'string') {
             return false;
         }
@@ -21,10 +29,10 @@ module.exports = class Maidenhead {
 
         var length = mlocation.length / 2;
 
-        for (var counter=0; counter<length;i++){
-            var grid = mlocation.substr(counter*2,2);
+        for (var counter = 0; counter < length; counter++) {
+            var grid = mlocation.substr(counter * 2, 2);
 
-            if (counter == 0){
+            if (counter == 0) {
                 if (grid.match(/[a-rA-R]{2}/) == null) {
                     return false;
                 }
@@ -42,66 +50,66 @@ module.exports = class Maidenhead {
         }
     }
 
-    static toLatLon(mlocation){
-        var maidenhead = new Maidenhead();
+    static toLatLon(mlocation: string) {
+        var maidenhead = new Maidenhead(0, 0);
         maidenhead.locator = mlocation;
 
         return [maidenhead.lat, maidenhead.lon];
     }
 
-    distanceTo(endLatLon, unit = 'km'){
+    distanceTo(endLatLon: Maidenhead, unit = 'km') {
         var r = 6371;
 
-        switch(unit) {
-           case 'm':
-               r *= 1000;
+        switch (unit) {
+            case 'm':
+                r *= 1000;
         }
 
         var hn = this._deg_to_rad(this.lat);
         var he = this._deg_to_rad(this.lon);
-        var n  = this._deg_to_rad(endLatLon.lat);
-        var e  = this._deg_to_rad(endLatLon.lon);
+        var n = this._deg_to_rad(endLatLon.lat);
+        var e = this._deg_to_rad(endLatLon.lon);
 
-        var co = Math.cos(he-e) * Math.cos(hn) * Math.cos(n) + Math.sin(hn) * Math.sin(n);
-        var ca = Math.atan(Math.abs(Math.sqrt(1-co*co) / co));
+        var co = Math.cos(he - e) * Math.cos(hn) * Math.cos(n) + Math.sin(hn) * Math.sin(n);
+        var ca = Math.atan(Math.abs(Math.sqrt(1 - co * co) / co));
 
-        if (co <0) {
+        if (co < 0) {
             ca = Math.PI - ca;
         }
 
         return r * ca;
     }
 
-    bearingTo(toHeading, compassBearing = false) {
+    bearingTo(toHeading: Maidenhead, compassBearing = false) {
         let hn = this._deg_to_rad(this.lat);
         let he = this._deg_to_rad(this.lon);
-        let n  = this._deg_to_rad(toHeading.lat);
-        let e  = this._deg_to_rad(toHeading.lon);
+        let n = this._deg_to_rad(toHeading.lat);
+        let e = this._deg_to_rad(toHeading.lon);
 
         let co = Math.cos(he - e) * Math.cos(hn) * Math.cos(n) + Math.sin(hn) * Math.sin(n);
-        let ca = Math.atan(Math.abs(Math.sqrt(1-co *co)/co));
+        let ca = Math.atan(Math.abs(Math.sqrt(1 - co * co) / co));
 
-        if(co <0) {
+        if (co < 0) {
             ca = Math.PI - ca
         }
 
-        let si = Math.sin(e-he) * Math.cos(n) * Math.cos(hn);
+        let si = Math.sin(e - he) * Math.cos(n) * Math.cos(hn);
         co = Math.sin(n) - Math.sin(hn) * Math.cos(ca);
-        let az = Math.atan(Math.abs(si/co));
+        let az = Math.atan(Math.abs(si / co));
 
         if (co < 0) {
             az = Math.PI - az;
         }
 
-        if(si < 0) {
+        if (si < 0) {
             az = -az;
         }
 
-        if(az < 0) {
+        if (az < 0) {
             az = az + 2 * Math.PI;
         }
 
-        var heading = Math.round(this._rad_to_deg(az));
+        var heading: string | number = Math.round(this._rad_to_deg(az));
 
         if (compassBearing) {
             heading = this._compass_bearing(heading);
@@ -110,23 +118,23 @@ module.exports = class Maidenhead {
         return heading;
     }
 
-    get lat(){
+    get lat() {
         return parseFloat(this._lat.toPrecision(6));
     }
 
-    set lat(pos){
+    set lat(pos) {
         this._lat = this._range_check("lat", 90.0, pos)
     }
 
-    get lon(){
+    get lon() {
         return parseFloat(this._lon.toPrecision(6));
     }
 
-    set lon(pos){
+    set lon(pos) {
         this._lon = this._range_check("lon", 180.0, pos)
     }
 
-    get precision(){
+    get precision() {
         return this._precision;
     }
 
@@ -134,7 +142,7 @@ module.exports = class Maidenhead {
         this._precision = p;
     }
 
-    set locator(mlocation){
+    set locator(mlocation) {
         if (!Maidenhead.valid(mlocation)) {
             throw "Location is not a valid Maidenhead Locator System string";
         }
@@ -152,7 +160,7 @@ module.exports = class Maidenhead {
         this._convert_part_to_latlon(4, 10 * 24 * 10 * 24);
     }
 
-    get locator(){
+    get locator() {
         this._locator = '';
         this.__lat = this.lat + 90.0;
         this.__lon = this.lon + 180.0;
@@ -166,25 +174,25 @@ module.exports = class Maidenhead {
     }
 
     _compass_bearing(heading) {
-        if (heading >= 0 && heading <= 360){
+        if (heading >= 0 && heading <= 360) {
             var compassBearings = [
-                {"label": "N", "start": 0, "end": 11},
-                {"label": "NNE", "start": 11, "end": 33},
-                {"label": "NE", "start": 34, "end": 56},
-                {"label": "ENE", "start": 57, "end": 78},
-                {"label": "E", "start": 79, "end": 101},
-                {"label": "ESE", "start": 102, "end": 123},
-                {"label": "SE", "start": 124, "end": 146},
-                {"label": "SSE", "start": 147, "end": 168},
-                {"label": "S", "start": 169, "end": 191},
-                {"label": "SSW", "start": 192, "end": 213},
-                {"label": "SW", "start": 214, "end": 236},
-                {"label": "WSW", "start": 237, "end": 258},
-                {"label": "W", "start":	259, "end": 281},
-                {"label": "WNW", "start": 282, "end": 303},
-                {"label": "NW", "start": 304, "end": 326},
-                {"label": "NNW", "start": 327, "end": 348},
-                {"label": "N", "start": 349, "end": 360}
+                { "label": "N", "start": 0, "end": 11 },
+                { "label": "NNE", "start": 11, "end": 33 },
+                { "label": "NE", "start": 34, "end": 56 },
+                { "label": "ENE", "start": 57, "end": 78 },
+                { "label": "E", "start": 79, "end": 101 },
+                { "label": "ESE", "start": 102, "end": 123 },
+                { "label": "SE", "start": 124, "end": 146 },
+                { "label": "SSE", "start": 147, "end": 168 },
+                { "label": "S", "start": 169, "end": 191 },
+                { "label": "SSW", "start": 192, "end": 213 },
+                { "label": "SW", "start": 214, "end": 236 },
+                { "label": "WSW", "start": 237, "end": 258 },
+                { "label": "W", "start": 259, "end": 281 },
+                { "label": "WNW", "start": 282, "end": 303 },
+                { "label": "NW", "start": 304, "end": 326 },
+                { "label": "NNW", "start": 327, "end": 348 },
+                { "label": "N", "start": 349, "end": 360 }
             ];
 
             var result = compassBearings.find(function (element, index, array) {
@@ -206,11 +214,11 @@ module.exports = class Maidenhead {
         return rad / Math.PI * 180;
     }
 
-    _pad_locator(){
-        var length = this._locator.length / 2 ;
+    _pad_locator() {
+        var length = this._locator.length / 2;
 
         while (length < 5) {
-            if ((length%2) == 1) {
+            if ((length % 2) == 1) {
                 this._locator += '55';
             }
             else {
@@ -223,7 +231,7 @@ module.exports = class Maidenhead {
 
     _range_check(target, range, pos) {
         pos = Number(pos);
-        if (pos < -range || pos > range){
+        if (pos < -range || pos > range) {
             throw target + " must be between -" + range + " and +" + range;
         }
 
@@ -231,23 +239,23 @@ module.exports = class Maidenhead {
     }
 
     _convert_part_to_latlon(counter, divisor) {
-        var grid_lon = this._locator.substr(counter*2, 1);
-        var grid_lat = this._locator.substr(counter*2+1, 1);
+        var grid_lon = this._locator.substr(counter * 2, 1);
+        var grid_lat = this._locator.substr(counter * 2 + 1, 1);
 
         this._lat += this._l2n(grid_lat) * 10.0 / divisor;
         this._lon += this._l2n(grid_lon) * 20.0 / divisor;
     }
 
-    _calculate_field(){
-        this.__lat = (this.__lat/10) + 0.0000001;
-        this.__lon = (this.__lon/20) + 0.0000001;
+    _calculate_field() {
+        this.__lat = (this.__lat / 10) + 0.0000001;
+        this.__lon = (this.__lon / 20) + 0.0000001;
         this._locator += this._n2l(Math.floor(this.__lon)).toUpperCase() + this._n2l(Math.floor(this.__lat)).toUpperCase();
         this.__precision -= 1;
     }
 
-    _calculate_values(){
-        for (let counter=0; counter < this.__precision;counter++) {
-            if((counter%2) == 0) {
+    _calculate_values() {
+        for (let counter = 0; counter < this.__precision; counter++) {
+            if ((counter % 2) == 0) {
                 this._compute_locator(counter, 10);
             } else {
                 this._compute_locator(counter, 24);
@@ -255,26 +263,26 @@ module.exports = class Maidenhead {
         }
     }
 
-    _compute_locator(counter, divisor){
+    _compute_locator(counter, divisor) {
         this.__lat = (this.__lat - Math.floor(this.__lat)) * divisor;
         this.__lon = (this.__lon - Math.floor(this.__lon)) * divisor;
 
-        if ((counter%2) == 0) {
+        if ((counter % 2) == 0) {
             this._locator += "" + Math.floor(this.__lon) + "" + Math.floor(this.__lat)
         } else {
             this._locator += this._n2l(Math.floor(this.__lon)) + this._n2l(Math.floor(this.__lat))
         }
     }
 
-    _l2n(letter){
+    _l2n(letter) {
         if (letter.match(/[0-9]+/)) {
             return parseInt(letter);
-        }else{
+        } else {
             return letter.toLowerCase().charCodeAt(0) - 97;
         }
 
     }
-    _n2l(number){
+    _n2l(number) {
         return String.fromCharCode(97 + number);
     }
 }
