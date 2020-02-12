3323ba52bed51f7efb91f6d631eb21d4dc6555af
Paul Irwin <paulirwin@gmail.com>
2020-02-12 10:14:09 -0700

    Add more unit tests; refactor Maidenhead class to rely less on internal state and be more functional

--- src/maidenhead.ts
+++ src/maidenhead.ts
@@ -1,76 +1,88 @@
+import { 
+    degreesToRadians, 
+    radiansToDegrees, 
+    compassBearing, 
+    rangeCheck, 
+    letterToNumber, 
+    numberToLetter 
+} from "./util";
+
+const locatorRegex = /^[A-R]{2}(([0-9]{2})([A-X]{2})?)?$/i;
 
 export default class Maidenhead {
     private _lat: number | undefined;
     private _lon: number | undefined;
-    private _precision: number = 5;
+    private _precision: number = 3;
     private _locator: string | undefined;
-    private __lat: number | undefined;
-    private __lon: number | undefined;
-    private __precision: number | undefined;
-
-    constructor(lat?: number, lon?: number, precision: number = 5) {
-        this.lat = lat;
-        this.lon = lon;
+    
+    constructor(lat?: number, lon?: number, precision: number = 3) {
+        this.latitude = lat;
+        this.longitude = lon;
         this.precision = precision;
     }
 
+    /**
+     * Validates that a grid code is a valid code.
+     * @param locator The grid code to validate.
+     * @returns Returns true if valid, false if invalid.
+     */
     static valid(locator: string) {
-        if (typeof locator !== 'string') {
-            return false;
-        }
-
-        if (locator.length < 2) {
-            return false;
-        }
-
-        if ((locator.length % 2) !== 0) {
-            return false;
-        }
-
-        var length = locator.length / 2;
-
-        for (var counter = 0; counter < length; counter++) {
-            var grid = locator.substr(counter * 2, 2);
-
-            if (counter == 0) {
-                if (grid.match(/[a-rA-R]{2}/) == null) {
-                    return false;
-                }
-            } else if ((counter % 2) == 0) {
-                if (grid.match(/[a-xA-X]{2}/) == null) {
-                    return false;
-                }
-            } else {
-                if (grid.match(/[0-9]{2}/) == null) {
-                    return false;
-                }
-            }
-            return true;
-        }
+        return locatorRegex.test(locator);
     }
 
+    /**
+     * Calculates the center point of the grid square as latitude/longitude.
+     * @param locator The grid code locator to convert to latitude/longitude
+     * @returns Returns an array of length 2 with [latitude, longitude] values.
+     */
     static toLatLon(locator: string): [number, number] {
-        var maidenhead = new Maidenhead();
-        maidenhead.locator = locator;
-
-        return [maidenhead.lat, maidenhead.lon];
+        const maidenhead = Maidenhead.fromLocator(locator);
+        return [maidenhead.latitude, maidenhead.longitude];
     }
 
-    distanceTo(other: Maidenhead, unit: 'km' | 'm' = 'km') {
-        var r = 6371;
+    /**
+     * Creates a new Maidenhead instance from the given grid locator code.
+     * @param locator The grid locator code.
+     * @param precision The precision of the locator.
+     * @returns Returns a new Maidenhead instance for the given grid code.
+     */
+    static fromLocator(locator: string, precision?: number): Maidenhead {
+        const maidenhead = new Maidenhead(undefined, undefined, precision || (locator.length / 2));
+        maidenhead.locator = locator;
+        return maidenhead;
+    }
+
+    /**
+     * Creates a new Maidenhead instance from the given latitude and longitude.
+     * @param latitude The latitude of the position.
+     * @param longitude The longitude of the position.
+     * @param precision The precision of the locator.
+     */
+    static fromCoordinates(latitude: number, longitude: number, precision: number = 3) {
+        return new Maidenhead(latitude, longitude, precision);
+    }
+
+    /**
+     * Calculates the distance between this point and another point.
+     * @param other The other point to calculate the distance to.
+     * @param unit The unit of distance in either "km" or "m".
+     * @returns Returns the distance between the two points in the specified unit.
+     */
+    distanceTo(other: Maidenhead, unit: 'km' | 'm' = 'km'): number {
+        let r = 6371;
 
         switch (unit) {
             case 'm':
                 r *= 1000;
         }
 
-        var hn = this._deg_to_rad(this.lat);
-        var he = this._deg_to_rad(this.lon);
-        var n = this._deg_to_rad(other.lat);
-        var e = this._deg_to_rad(other.lon);
+        const hn = degreesToRadians(this.latitude);
+        const he = degreesToRadians(this.longitude);
+        const n = degreesToRadians(other.latitude);
+        const e = degreesToRadians(other.longitude);
 
-        var co = Math.cos(he - e) * Math.cos(hn) * Math.cos(n) + Math.sin(hn) * Math.sin(n);
-        var ca = Math.atan(Math.abs(Math.sqrt(1 - co * co) / co));
+        const co = Math.cos(he - e) * Math.cos(hn) * Math.cos(n) + Math.sin(hn) * Math.sin(n);
+        let ca = Math.atan(Math.abs(Math.sqrt(1 - co * co) / co));
 
         if (co < 0) {
             ca = Math.PI - ca;
@@ -79,11 +91,16 @@ export default class Maidenhead {
         return r * ca;
     }
 
+    /**
+     * Calculates the bearing from this point to another point.
+     * @param other The other point to calculate the bearing to.
+     * @returns Returns the bearing to the other location in degrees.
+     */
     bearingTo(other: Maidenhead): number {
-        let hn = this._deg_to_rad(this.lat);
-        let he = this._deg_to_rad(this.lon);
-        let n = this._deg_to_rad(other.lat);
-        let e = this._deg_to_rad(other.lon);
+        const hn = degreesToRadians(this.latitude);
+        const he = degreesToRadians(this.longitude);
+        const n = degreesToRadians(other.latitude);
+        const e = degreesToRadians(other.longitude);
 
         let co = Math.cos(he - e) * Math.cos(hn) * Math.cos(n) + Math.sin(hn) * Math.sin(n);
         let ca = Math.atan(Math.abs(Math.sqrt(1 - co * co) / co));
@@ -92,7 +109,7 @@ export default class Maidenhead {
             ca = Math.PI - ca
         }
 
-        let si = Math.sin(e - he) * Math.cos(n) * Math.cos(hn);
+        const si = Math.sin(e - he) * Math.cos(n) * Math.cos(hn);
         co = Math.sin(n) - Math.sin(hn) * Math.cos(ca);
         let az = Math.atan(Math.abs(si / co));
 
@@ -108,182 +125,175 @@ export default class Maidenhead {
             az = az + 2 * Math.PI;
         }
 
-        return Math.round(this._rad_to_deg(az));
+        return Math.round(radiansToDegrees(az));
     }
 
+    /**
+     * Calculates the compass bearing (i.e. "NNW") from this point to another point.
+     * @param other The other point to calculate the bearing to.
+     * @returns Returns the compass bearing string.
+     */
     compassBearingTo(other: Maidenhead): string {
         const heading = this.bearingTo(other);
 
-        return this._compass_bearing(heading);
+        return compassBearing(heading);
     }
 
-    get lat(): number {
+    /**
+     * Gets the latitude of this locator.
+     */
+    get latitude(): number {
         return parseFloat(this._lat.toPrecision(6));
     }
 
-    set lat(pos: number) {
-        this._lat = this._range_check("lat", 90.0, pos)
+    /**
+     * Sets the latitude of this locator.
+     */
+    set latitude(pos: number) {
+        this._lat = rangeCheck("lat", 90.0, pos);
+        this._locator = null;
     }
 
-    get lon(): number {
+    /**
+     * Gets the longitude of this locator.
+     */
+    get longitude(): number {
         return parseFloat(this._lon.toPrecision(6));
     }
 
-    set lon(pos: number) {
-        this._lon = this._range_check("lon", 180.0, pos)
+    /**
+     * Sets the longitude of this locator.
+     */
+    set longitude(pos: number) {
+        this._lon = rangeCheck("lon", 180.0, pos);
+        this._locator = null;
     }
 
+    /**
+     * Gets the precision of this locator.
+     */
     get precision(): number {
         return this._precision;
     }
 
+    /**
+     * Sets the precision of this locator.
+     */
     set precision(p: number) {
+        if (p < 1 || p > 5) {
+            throw "Precision value must be in the range of 1 to 5.";
+        }
+
         this._precision = p;
+        this._locator = null;
     }
 
+    /**
+     * Sets the grid locator code of this position.
+     */
     set locator(value: string) {
         if (!Maidenhead.valid(value)) {
             throw "Location is not a valid Maidenhead Locator System string";
         }
 
         this._locator = value;
-        this._lat = -90.00;
-        this._lon = -180.00;
+        this._precision = value.length / 2;
 
-        this._pad_locator();
+        const [ lat, lon ] = Maidenhead.gridCodeToCoordinates(value);
 
-        this._convert_part_to_latlon(0, 1);
-        this._convert_part_to_latlon(1, 10);
-        this._convert_part_to_latlon(2, 10 * 24);
-        this._convert_part_to_latlon(3, 10 * 24 * 10);
-        this._convert_part_to_latlon(4, 10 * 24 * 10 * 24);
+        this._lat = lat;
+        this._lon = lon;
     }
 
+    /**
+     * Gets the grid locator code of this position.
+     */
     get locator(): string {
-        this._locator = '';
-        this.__lat = this.lat + 90.0;
-        this.__lon = this.lon + 180.0;
-        this.__precision = this.precision;
+        if (this._locator && this._locator.length) {
+            return this._locator;
+        }
 
-
-        this._calculate_field();
-        this._calculate_values();
+        this._locator = Maidenhead.coordinatesToGridCode(this.latitude, this.longitude, this.precision);
 
         return this._locator;
     }
 
-    private _compass_bearing(heading: number): string {
-        if (heading >= 0 && heading <= 360) {
-            var compassBearings = [
-                { label: "N", start: 0, end: 11 },
-                { label: "NNE", start: 11, end: 33 },
-                { label: "NE", start: 34, end: 56 },
-                { label: "ENE", start: 57, end: 78 },
-                { label: "E", start: 79, end: 101 },
-                { label: "ESE", start: 102, end: 123 },
-                { label: "SE", start: 124, end: 146 },
-                { label: "SSE", start: 147, end: 168 },
-                { label: "S", start: 169, end: 191 },
-                { label: "SSW", start: 192, end: 213 },
-                { label: "SW", start: 214, end: 236 },
-                { label: "WSW", start: 237, end: 258 },
-                { label: "W", start: 259, end: 281 },
-                { label: "WNW", start: 282, end: 303 },
-                { label: "NW", start: 304, end: 326 },
-                { label: "NNW", start: 327, end: 348 },
-                { label: "N", start: 349, end: 360 }
-            ];
+    private static gridCodeToCoordinates(locator: string): [number, number] {
+        let lat = -90.00;
+        let lon = -180.00;
 
-            var result = compassBearings.find(element => {
-                if (heading > element.start && heading < element.end) {
-                    return true;
-                }
-                return false;
-            });
+        for (let counter = 0; counter < (locator.length / 2); counter++) {
+            let divisor;
 
-            return result ? result.label : '';
-        }
-
-        return '';
-    }
-
-    private _deg_to_rad(deg: number): number {
-        return deg / 180 * Math.PI;
-    }
-
-    private _rad_to_deg(rad: number): number {
-        return rad / Math.PI * 180;
-    }
-
-    private _pad_locator(): void {
-        var length = this._locator.length / 2;
-
-        while (length < 5) {
-            if ((length % 2) == 1) {
-                this._locator += '55';
-            }
-            else {
-                this._locator += 'LL';
+            switch (counter) {
+                case 0:
+                    divisor = 1;
+                    break;
+                case 1:
+                    divisor = 10;
+                    break;
+                case 2:
+                    divisor = 10 * 24;
+                    break;
+                case 3:
+                    divisor = 10 * 24 * 10;
+                    break;
+                case 4:
+                    divisor = 10 * 24 * 10 * 24;
             }
 
-            length = this._locator.length / 2;
-        }
-    }
-
-    private _range_check(target: string, range: number, pos: number): number {
-        pos = Number(pos);
-        if (pos < -range || pos > range) {
-            throw target + " must be between -" + range + " and +" + range;
+            const [lat2, lon2] = Maidenhead._convert_part_to_latlon(locator, counter, divisor);
+            lat += lat2;
+            lon += lon2;
         }
 
-        return pos;
+        return [lat, lon];
     }
 
-    private _convert_part_to_latlon(counter: number, divisor: number): void {
-        var grid_lon = this._locator.substr(counter * 2, 1);
-        var grid_lat = this._locator.substr(counter * 2 + 1, 1);
+    private static _convert_part_to_latlon(locator: string, counter: number, divisor: number): [number, number] {
+        let midpoint = 0;
 
-        this._lat += this._l2n(grid_lat) * 10.0 / divisor;
-        this._lon += this._l2n(grid_lon) * 20.0 / divisor;
+        if (locator.length < (counter + 1) * 2) {
+            return;
+        } else if (locator.length === (counter + 1) * 2) {
+            midpoint = 0.5;
+        }
+
+        const grid_lon = locator.substr(counter * 2, 1);
+        const grid_lat = locator.substr(counter * 2 + 1, 1);
+
+        const lat = (letterToNumber(grid_lat) + midpoint) * 10.0 / divisor;
+        const lon = (letterToNumber(grid_lon) + midpoint) * 20.0 / divisor;
+
+        return [lat, lon];
     }
 
-    private _calculate_field(): void {
-        this.__lat = (this.__lat / 10) + 0.0000001;
-        this.__lon = (this.__lon / 20) + 0.0000001;
-        this._locator += this._n2l(Math.floor(this.__lon)).toUpperCase() + this._n2l(Math.floor(this.__lat)).toUpperCase();
-        this.__precision -= 1;
-    }
+    /**
+     * Converts coordinates to a maidenhead grid code.
+     * @param latitude The latitude of the coordinate.
+     * @param longitude The longitude of the coordinate.
+     * @param precision The precision of the grid code, in the range 1-5. Defaults to 3 for a 6-character code.
+     * @returns Returns the grid code, twice the length of the specified precision.
+     */
+    static coordinatesToGridCode(latitude: number, longitude: number, precision: number = 3): string {
+        let lat = ((latitude + 90.0) / 10) + 0.0000001;
+        let lon = ((longitude + 180.0) / 20) + 0.0000001;
 
-    private _calculate_values(): void {
-        for (let counter = 0; counter < this.__precision; counter++) {
+        let locator = numberToLetter(Math.floor(lon)).toUpperCase() + numberToLetter(Math.floor(lat)).toUpperCase();
+
+        for (let counter = 0; counter < precision - 1; counter++) {
+            const divisor = (counter % 2) === 0 ? 10 : 24;
+            
+            lat = (lat - Math.floor(lat)) * divisor;
+            lon = (lon - Math.floor(lon)) * divisor;
+            
             if ((counter % 2) == 0) {
-                this._compute_locator(counter, 10);
+                locator += Math.floor(lon).toString() + Math.floor(lat).toString();
             } else {
-                this._compute_locator(counter, 24);
+                locator += numberToLetter(Math.floor(lon)) + numberToLetter(Math.floor(lat));
             }
         }
-    }
 
-    private _compute_locator(counter: number, divisor: number): void {
-        this.__lat = (this.__lat - Math.floor(this.__lat)) * divisor;
-        this.__lon = (this.__lon - Math.floor(this.__lon)) * divisor;
-
-        if ((counter % 2) == 0) {
-            this._locator += "" + Math.floor(this.__lon) + "" + Math.floor(this.__lat)
-        } else {
-            this._locator += this._n2l(Math.floor(this.__lon)) + this._n2l(Math.floor(this.__lat))
-        }
-    }
-
-    private _l2n(letter: string): number {
-        if (letter.match(/[0-9]+/)) {
-            return parseInt(letter);
-        } else {
-            return letter.toLowerCase().charCodeAt(0) - 97;
-        }
-    }
-
-    private _n2l(number: number): string {
-        return String.fromCharCode(97 + number);
+        return locator;
     }
 }
